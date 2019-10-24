c3f1f75bd3044cd179b416c2141d29d8dfa203bd
Omar Nasr <nasr.omar@icloud.com>
2019-10-24 19:48:29 +0200

    added comments

--- js/animateOnScroll.js
+++ js/animateOnScroll.js
@@ -1,18 +1,17 @@
 // JavaScript Document
 
-
+// Fade Left
 const fadeLeft = document.querySelectorAll(".hidden-fade-left");
 
 function activeFadeLeft() {
 	fadeLeft.forEach(hiddenFadeLeft => {
 		var positionFromTop = hiddenFadeLeft.getBoundingClientRect().top;
-		if ((positionFromTop * 1.2) - window.innerHeight <= 0) {
+		if ((positionFromTop * 1.2) - window.innerHeight <= 0) { // adds active-fade-left once in ~ middle of div
 			hiddenFadeLeft.classList.add("active-fade-left");
-		} else if (positionFromTop - window.innerHeight >= 0) {
+		} else if (positionFromTop - window.innerHeight >= 0) { // remove active-fade-left once scrolled above top of div
 			hiddenFadeLeft.classList.remove("active-fade-left");
 		}
 	});
 }
-
 
 window.addEventListener("scroll", activeFadeLeft);
