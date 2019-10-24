60a24a41045ef26c8362849af256051bc41fe3b6
Omar Nasr <nasr.omar@icloud.com>
2019-10-24 19:46:12 +0200

    javascript for fade left and styles adjusted

--- js/animateOnScroll.js
+++ js/animateOnScroll.js
@@ -1,1 +1,18 @@
 // JavaScript Document
+
+
+const fadeLeft = document.querySelectorAll(".hidden-fade-left");
+
+function activeFadeLeft() {
+	fadeLeft.forEach(hiddenFadeLeft => {
+		var positionFromTop = hiddenFadeLeft.getBoundingClientRect().top;
+		if ((positionFromTop * 1.2) - window.innerHeight <= 0) {
+			hiddenFadeLeft.classList.add("active-fade-left");
+		} else if (positionFromTop - window.innerHeight >= 0) {
+			hiddenFadeLeft.classList.remove("active-fade-left");
+		}
+	});
+}
+
+
+window.addEventListener("scroll", activeFadeLeft);
